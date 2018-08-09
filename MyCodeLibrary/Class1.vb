4470deed6a7b31277d900fb43169ec9439d24546
Travis Jones <travisj_jones@hotmail.com>
2018-08-09 14:07:07 -0500

    Add project files.

---
+++ MyCodeLibrary/Class1.vb
@@ -0,0 +1,18 @@
+﻿Imports System.IO
+Imports System.Net
+
+Public Class Scrape
+
+    Public Function ScrapeWebpage(ByVal url As String,
+                                  ByVal path As String) As String
+
+        Dim client As New WebClient()
+        Dim reply As String = client.DownloadString(url)
+
+        File.WriteAllText(path, reply)
+
+        Return reply
+
+    End Function
+
+End Class
---
+++ WebScraper/Module1.vb
@@ -0,0 +1,16 @@
+﻿Imports MyCodeLibrary
+
+Module Module1
+
+    Sub Main()
+
+        Dim myScrape = New Scrape()
+        Dim result As String = myScrape.ScrapeWebpage("http://www.microsoft.com",
+                                                      "C:\Example\mirosoft.txt")
+
+        Console.WriteLine(result)
+        Console.ReadLine()
+
+    End Sub
+
+End Module
